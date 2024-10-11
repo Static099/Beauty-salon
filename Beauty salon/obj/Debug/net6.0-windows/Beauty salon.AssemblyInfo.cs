71a97cb82dadcca2a0a822b00e7cce572583363e
passiveentity <t.i.t.o.v.a.a@yandex.ru>
2024-10-11 14:40:00 +0300

    Форма авторизации

--- Beauty salon/obj/Debug/net6.0-windows/Beauty salon.AssemblyInfo.cs
+++ Beauty salon/obj/Debug/net6.0-windows/Beauty salon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Beauty salon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6407bdf8d9a38f7e89be85074d6b2c0a51252f5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7872a0fa455ed032bf12c8afb1ad03caf6654d16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Beauty salon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Beauty salon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
